9c5b800813fab028abf83d7f3886a72f4128c1c1
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2025-07-21 17:48:05 +0530

    Add my own Open WebUI flake

--- flake.nix
+++ flake.nix
@@ -3,36 +3,42 @@
 
     inputs = {
         
-          # Nixpkgs
-          nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-          nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+        # Nixpkgs
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
-          # Home-Manager
-          home-manager = {
-              url = "github:nix-community/home-manager/release-25.05";
-              inputs.nixpkgs.follows = "nixpkgs";
-          };
+        # Home-Manager
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
-          # Hyprland
-          hyprland = {
-              url = "github:hyprwm/Hyprland";
-              inputs.nixpkgs.follows = "nixpkgs";
-          };
+        # Hyprland
+        hyprland = {
+            url = "github:hyprwm/Hyprland";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
-          # Sops-Nix
-          sops-nix = {
-              url = "github:Mic92/sops-nix";
-              inputs.nixpkgs.follows = "nixpkgs";
-          };
+        # Sops-Nix
+        sops-nix = {
+            url = "github:Mic92/sops-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
-          # Zen-Browser
-          zen-browser = {
-              url = "github:SoumyabrataBanik/flake-zen-browser";
-              inputs.nixpkgs.follows = "nixpkgs";
-          };
+        # Zen-Browser
+        zen-browser = {
+            url = "github:SoumyabrataBanik/flake-zen-browser";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        # Open-Webui
+        open-webui = {
+            url = "github:SoumyabrataBanik/open-webui-flake";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, zen-browser, ... }@inputs:
+    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, zen-browser, open-webui, ... }@inputs:
         let
             lib = nixpkgs.lib;
             system = "x86_64-linux";
@@ -49,6 +55,7 @@
                         nixpkgs.overlays = [
                             (final: prev: {
                                 zen-browser = zen-browser.packages.${prev.system}.zen-browser;
+                                open-webui = open-webui.packages.${prev.system}.default;
                             })
                         ];
                     })
--- home/hiskingisdone/default.nix
+++ home/hiskingisdone/default.nix
@@ -8,6 +8,7 @@
         ./git.nix
         ./gtk.nix
         ./languages.nix
+        ./open-webui.nix
         ./programs.nix
         ./rclone.nix
         ./services.nix
