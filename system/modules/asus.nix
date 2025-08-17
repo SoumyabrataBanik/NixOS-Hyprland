15e0897c4e2756de207ec807e77ee85ed26ee190
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2025-08-17 11:14:44 +0530

    Configure asusd. Doesn't work as Asusd generates its own config on boot.

--- system/modules/asus.nix
+++ system/modules/asus.nix
@@ -4,6 +4,18 @@
   services = {
     asusd = {
       enable = true;
+      asusdConfig = {
+        text = ''
+          (
+            bat_charge_limit: 100,
+            charge_control_end_threshold: 100,
+            ac_command: "",
+            bat_command: "",
+            ac_profile_tunings: {},
+            dc_profile_tunings: {},
+          )
+        '';
+      };
     };
     power-profiles-daemon.enable = true;
   };
