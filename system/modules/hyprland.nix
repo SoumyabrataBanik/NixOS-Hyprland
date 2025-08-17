848fcd3bef2ce9ae45aa89e43d2a30436283aab4
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2025-08-17 19:26:28 +0530

    Add package definition to prevent version collision with Home-Manager

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -1,42 +1,48 @@
-{ config, pkgs, pkgs-unstable, ... }:
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 
 {
-    environment.systemPackages = with pkgs-unstable; [
-        hyprpaper
-        hyprlock
-        hypridle
-        hyprshot
-        rofi-wayland
-        wlogout
-        wl-clipboard
-        dunst
-        qt5.qtwayland
-        qt6.qtwayland
-        grim
-        waybar
-        networkmanagerapplet
-        wireplumber
-        xfce.thunar
-        xdg-desktop-portal
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-wlr
-        swaynotificationcenter
-    ];
+  environment.systemPackages = with pkgs-unstable; [
+    hyprpaper
+    hyprlock
+    hypridle
+    hyprshot
+    rofi-wayland
+    wlogout
+    wl-clipboard
+    dunst
+    qt5.qtwayland
+    qt6.qtwayland
+    grim
+    waybar
+    networkmanagerapplet
+    wireplumber
+    xfce.thunar
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
+    swaynotificationcenter
+  ];
 
-    programs = {
-        hyprland = {
-            enable = true;
-            xwayland.enable = true;
-        };
-
-        thunar = {
-            enable = true;
-            plugins = with pkgs.xfce; [ thunar-volman ];
-        };
+  programs = {
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      package = pkgs-unstable.hyprland;
     };
 
-    services.blueman.enable = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [ thunar-volman ];
+    };
+  };
 
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  services.blueman.enable = true;
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
 }
