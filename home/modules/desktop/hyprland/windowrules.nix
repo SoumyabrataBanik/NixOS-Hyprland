075542bcb9c378cde35b7fc253db2e4748914c82
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2025-08-19 21:01:43 +0530

    feat: add windowrule to override default opacity for zen

--- home/modules/desktop/hyprland/windowrules.nix
+++ home/modules/desktop/hyprland/windowrules.nix
@@ -6,6 +6,7 @@
       "suppressevent maximize, class:.*"
       "noblur,class:^()$,title:^()$"
       "opacity 0.8 override 0.6 override 0.9 override,class:^(kitty)$"
+      "opacity 1 override 1 override 1 override,class:^(zen)$"
     ];
   };
 }
